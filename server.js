f89d579f65f4de421de5b2d9dc9494b6283c5969
developart16 <artur.develop@gmail.com>
2022-08-12 14:45:56 +0200

    ejercicio 5: endpoint que devuelva lo de hoy

--- server.js
+++ server.js
@@ -4,11 +4,23 @@ const app = express()
 const port = process.env.PORT ?? 3000;
 const mongoose = require('mongoose');
 const { version } = require("./package.json");
+const { dailyGenerate } = require('./scraper');
 
 mongoose.connect( (process.env.MONGO_URI + '/news') ?? 'mongodb://localhost:27017/news');
 
 app.use( express.json() )
 require('./Feed/routes')(app)
+
+
+app.post('/generateDaily', async (req, res)=>{
+
+    const generateds = await dailyGenerate();
+    return res.status(200).send({
+        ok: true,
+        todaysNews: generateds
+    })
+
+})
 
 app.get('/', (req, res) => {
     return res.status(200).send({
